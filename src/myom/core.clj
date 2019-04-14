abc7be966597ac3b179e0e8a8b7aadcfa2e73109
Joel Gluth <joel@gluth.io>
2019-04-14 18:34:01 +0100

    Zoom into new centre with left-click

--- src/myom/core.clj
+++ src/myom/core.clj
@@ -6,16 +6,22 @@
 
 (deftype complex [^double r ^double i])
 
-(def ^:const WIDTH  512)
-(def ^:const HEIGHT 512)
+(def ^:const WIDTH  800)
+(def ^:const HEIGHT 600)
 
 ; arbitrary divergence threshold
 (def ^:const THRESHOLD 4.0)
-(def ^:const ITERATIONS 40)
+
+;; 10 is visibly not enough at 512x512
+(def ^:const ITERATIONS 80)
+
+;; When zooming, expand this portion of the original image to fill
+;; smaller is zoomier
+(def ^:const ZOOM_RATIO 0.5)
 
 (defn setup []
 
-  (q/frame-rate 1)
+  (q/frame-rate 0.5)
 
   {:bottom -1.5
    :left   -2.5
@@ -23,7 +29,26 @@
    :right   1.5})
 
 (defn update-state [state]
-  state)
+  (if  (q/mouse-pressed?) ;; zoom and recenter
+    (let [{:keys [bottom left top right]} state
+          slice-width (- right left)
+          slice-height (- top bottom)
+          mx-abs (/ (q/mouse-x) WIDTH)
+          my-abs (/ (q/mouse-y) HEIGHT)
+          new-center-x (+ left (* slice-width mx-abs))
+          new-center-y (- top (* slice-height my-abs))
+          new-slice-width (* ZOOM_RATIO slice-width)
+          new-slice-height (* ZOOM_RATIO slice-height)
+          new-left (- new-center-x (/ new-slice-width 2))
+          new-right (+ new-center-x (/ new-slice-width 2))
+          new-top (+ new-center-y (/ new-slice-height 2))
+          new-bottom (- new-center-y (/ new-slice-height 2))]
+
+      {:bottom new-bottom
+       :left new-left
+       :top new-top
+       :right new-right})
+    state))
 
 
 (defn cmul [^complex a ^complex b]
@@ -46,35 +71,36 @@
 (defn step [^complex z ^complex c]
   (cadd (cmul z z) c))
 
-(defn iterations-before-divergence [^complex c]
+(defn iterations-before-divergence [^complex c threshold max-iterations]
   (loop [i 0
          z (complex. 0 0)]
-    ;; (println "(diverges loop: " i z c)
-    (if (= i ITERATIONS)
+    (if (= i max-iterations)
       i
-      (if (> (magnitude z) THRESHOLD)
+      (if (> (magnitude z) threshold)
         i
         (recur (inc i) (step z c))))))
 
-(defn get-pixel-colour [bottom top left right x y]
-  ;; (println "(get-pixel-colour " bottom top left right x y ")")
+(defn get-pixel-colour [bottom top left right x y image-width image-height threshold max-iterations]
   (let [width (- right left)
         height (- top bottom)
-        wstep (/ width WIDTH)
-        hstep (/ height HEIGHT)
+        wstep (/ width image-width)
+        hstep (/ height image-height)
         a (+ left (* x wstep))
         b (- top (* y hstep))]
-    (let [i (iterations-before-divergence (complex. a b))
-          intensity (* i (/ 255 ITERATIONS))]
-      [intensity intensity intensity])))
+    (let [i (iterations-before-divergence (complex. a b) threshold max-iterations)
+          intensity (if (= i max-iterations)
+                      0
+                      (* i (/ 255 max-iterations)))]
+      (q/color intensity intensity intensity))))
 
 (defn draw-state [state]
+  (println "draw-state " state)
   (let [start (System/currentTimeMillis)
         i (q/create-image WIDTH HEIGHT :rgb)
         {:keys [bottom left top right]} state]
     (dotimes [x WIDTH]
       (dotimes [y HEIGHT]
-               (q/set-pixel i x y (apply q/color (get-pixel-colour bottom top left right x y)))))
+        (q/set-pixel i x y (get-pixel-colour bottom top left right x y WIDTH HEIGHT THRESHOLD ITERATIONS))))
     (q/image i 0 0)
     (println "render time: " (- (System/currentTimeMillis) start) "ms"))
 )
